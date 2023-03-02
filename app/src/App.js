53f3c5d89295462b0f37fc7fc70287306d7ea287
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-03-02 21:11:07 +0000

    Zombie brain tutorial swipe **This is not my work**

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,55 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import './App.css';
 
 function App() {
-    const [toDoTask, setToDoTask] = useState([]);
-    const [count, setCount] = useState(toDoTask.length);
-    const [inputValue, setInputValue] = useState('');
+    const [newItem, setNewItem] = useState("");
+    const [items, setItems] = useState([]);
+    
+
+    function addItem() {
+
+        if (!newItem) {
+            alert("Add an item!")
+            return;
+        }
+
+        const item = {
+            id: Math.floor(Math.random()* 1000),
+            value: newItem
+        };
+
+        setItems(oldList => [...oldList, item]);
+        setNewItem("");
+
+    }
+
+    function deleteItem(id) {
+        const newArray = items.filter(item => item.id !=+ id);
+        setItems(newArray);
+    }
 
     return (
-        <div>
-            <h1>To-Do List</h1>
-            <input type="text" placeholder="Type To-Do Task here..."></input>
-            <button>Add To-Do Task</button>
-            <ul>To-Do Task List
-                <li>To-Do Task</li>
-                <button>Check Box</button>
-                <button>Delete To-Do Task</button>
+        <div className="App">
+            <h1>Todo List</h1>
+
+            <input type="text"
+            placeholder="Add an item..."
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+            />
+
+            <button className="add-button" onClick={() => addItem()}>Add</button>
+
+            <ul>
+                {items.map(item => {
+                    return(
+                        <li key={item.id}><button className='delete-button' onClick={() => deleteItem(item.id)}>X</button> {item.value} <button className='delete-button' onClick={() => deleteItem(item.id)}>X</button>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
 }
+
+export default App
